26a39fcad415801394108aa79e4756be331d6d1c
erlandsorf <erland.sorflaten@gmail.com>
2025-01-27 14:39:53 +0100

    some shit

--- src/matlabprototyping/makeM.m
+++ src/matlabprototyping/makeM.m
@@ -1,13 +1,12 @@
-function Mmatrix = makeM(framenumber, framelist)
+function M = makeM(framenumber, framelist)
 %UNTITLED8 Summary of this function goes here
 %   Detailed explanation goes here
-    Mmatrix = sym(zeros(framenumber*6,framenumber*6));
-    mass = framelist(framenumber).mass;
-    massI = mass .* eye(3);
-
+    
+    M = sym(zeros(framenumber*6,framenumber*6));
+    massI = framelist(framenumber) .* eye(3);
     for i = 1:framenumber
-        Mmatrix(i*6-5:i*6-3,i*6-5:i*6-3) = massI;
-        Mmatrix(i*6-2:i*6,i*6-2:i*6) = framelist(framenumber).Jmatrix;
+        M(i*6-5:i*6-3,i*6-5:i*6-3) = massI
+        M(i*6-2:i*6,i*6-2:i*6); = framelist(framenumber).Jmatrix;
     end
     
 end
