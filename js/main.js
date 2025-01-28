b56c0435f7aeb05a990e29c54e99eb7452b61dbe
Norvat <62434024+Norvat@users.noreply.github.com>
2025-01-28 12:54:35 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,31 @@
-import * as THREE from '../libs/three/three.module.js';
-import { OrbitControls } from '../libs/three/controls/OrbitControls.js';
+import * as THREE from '/three/three.module.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / 2 / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+function main() {
 
-const graphicsDiv = document.getElementById("graphics");
-renderer.setSize(graphicsDiv.clientWidth, graphicsDiv.clientHeight);
-graphicsDiv.appendChild(renderer.domElement);
+    const canvas = document.querySelector( '#c' );
+    const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
 
-const BoxGeometry = new THREE.BoxGeometry();
-const BoxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(BoxGeometry, BoxMaterial)
-scene.add(cube);
+    const fov = 75;
+    const aspect = 2; // the canvas default
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.z = 2;
 
-camera.position.z = 2;
+    const scene = new THREE.Scene();
 
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
 
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+    const material = new THREE.MeshBasicMaterial( { color: 0x44aa88 } ); // greenish blue
+
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    renderer.render( scene, camera );
+
 }
-animate();
 
-// Adjust on resize
-window.addEventListener("resize", () => {
-    renderer.setSize(graphicsDiv.clientWidth, graphicsDiv.clientHeight);
-    camera.aspect = graphicsDiv.clientWidth / graphicsDiv.clientHeight;
-    camera.updateProjectionMatrix();
-});
+main();
