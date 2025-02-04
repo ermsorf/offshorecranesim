8aeb42d9079a92795216fd96660d178ee010bcff
Norvat <62434024+Norvat@users.noreply.github.com>
2025-02-04 21:19:25 +0100

    Begun coding a test pendulum

---
+++ CollisionDetection/MFMPendulumTest.m
@@ -0,0 +1,22 @@
+%% Test code for å pendulum to insert data into the collision detection algorithm. 
+
+% Length vector
+
+% Acceleration Vector
+
+% Velocity vector
+
+% Load J matrix
+
+% Mass load
+
+% Wire J matrix (Might not be nessesary) 
+
+% Mass Wire
+
+% Rotation matrix 1
+
+% Rotation matrix 2
+
+% Time step
+
